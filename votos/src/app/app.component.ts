e4c97f93347bb13e39564af8dacd6eecfa589209
Santiago <xoyonsantiago@gmail.com>
2019-04-10 11:14:19 -0600

    Creacion Splash

--- votos/src/app/app.component.ts
+++ votos/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, ModalController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { SplashPage } from '../pages/splash/splash';
 
 @Component({
   templateUrl: 'app.html'
@@ -16,7 +17,15 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, modalCtrl: ModalController) {
+    platform.ready().then(() => {
+
+      statusBar.styleDefault();
+
+      let splash = modalCtrl.create(SplashPage);
+      splash.present();
+
+    });
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -32,7 +41,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      // this.splashScreen.hide();
     });
   }
 
